54e76e8ff4d228674cf22b9fdda19cbe36ad03e2
PedroPontes <pedrosousapontes@gmail.com>
2025-05-25 09:54:41 +0100

    Updated python_classes.py

--- python_classes.py
+++ python_classes.py
@@ -4,5 +4,15 @@ class Person:
       self.name = name
       self.age = age
       self.classroom = classroom
-   def display_info():
-       print(self.name + ' ' + self.age + ' ' + self.classroom)
+    def display_info(self):
+       print(self.name + ' ' + self.age)
+
+class Student(Person):
+    def __init__(self, name, age, classroom):
+       Person.__init__(self, name, age)
+       self.classroom = classroom
+    def display_info(self):
+      print(self.name + ' ' + self.age + ' ' + self.classroom)
+      
+      
+      
