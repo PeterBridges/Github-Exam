1922b7fad0554b6c7e945db6ef64f51e5aa9157e
Pedro Pontes <47028187+PeterBridges@users.noreply.github.com>
2025-05-25 09:30:04 +0100

    Create python_classes.py
    Class Person and display_info method

---
+++ python_classes.py
@@ -0,0 +1,8 @@
+
+class Person:
+    def __init__(self, name , age, classroom):
+      self.name = name
+      self.age = age
+      self.classroom = classroom
+   def display_info():
+       print(self.name + ' ' + self.age + ' ' + self.classroom)
